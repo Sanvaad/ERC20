1a34534e45c825400a73a8398bc369b58208e557
Sanvaad <houseofsanvaad@gmail.com>
2024-12-29 14:30:53 +0530

    second sommit

---
+++ test/OurTokenTest.t.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+interface MintableToken {
+    function mint(address, uint256) external;
+}
+
+contract OurTokenTest is Test {
+    OurToken public ourToken;
+    DeployOurToken public deployer;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        deployer = new DeployOurToken();
+        ourToken = deployer.run();
+
+        vm.prank(msg.sender);
+        ourToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public {
+        assertEq(STARTING_BALANCE, ourToken.balanceOf(bob));
+    }
+
+    function testUsersCantMint() public {
+        vm.expectRevert();
+        MintableToken(address(ourToken)).mint(address(this), 1);
+    }
+
+    function testInitialSupply() public {
+        assertEq(ourToken.totalSupply(), deployer.INITIAL_SUPPLY());
+    }
+
+    function testAllowancesWorks() public {
+        uint256 initialAllowance = 1000;
+
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+
+        uint256 transferAmount = 500;
+
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
+
+    function testTransferFromRevertsIfExceedsAllowance() public {
+        vm.prank(bob);
+        ourToken.approve(alice, 100 ether);
+
+        vm.prank(alice);
+        vm.expectRevert();
+        ourToken.transferFrom(bob, alice, 200 ether);
+    }
+}
